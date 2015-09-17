d23e1101752d1c3343034adae5c3e47750fa6558
tonussi <lptonussi@gmail.com>
2015-09-17 17:39:33 -0300

    Update index

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 // Register the monolog logging service
+
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => 'php://stderr'
 ));
 
 // Register view rendering
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => dirname(__FILE__) . '/web/views'
+    'twig.path' => dirname(__FILE__) . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'views'
 ));
 
 // Our web handlers
