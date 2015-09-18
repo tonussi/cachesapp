5cd4cea5217162bef76539b934d665227dcb42b6
tonussi <lptonussi@gmail.com>
2015-09-17 21:07:18 -0300

    Add hash session

--- gethint.php
+++ gethint.php
@@ -21,12 +21,33 @@
  * @copyright 2015 Universidade Federal de Santa Catarina {@link http://ufsc.br/}
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Cache.php';
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'CacheCalculadoraImpl.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// $h is the cleaned value of $_POST["hash"]
 
+if (isset($_SESSION["hash"]) && is_array($_SESSION["hash"])) {
+    if (in_array($h, $_SESSION["hash"])) {
+        // duplicate form submission
+        /* REDIRECT SOMEWHERE HERE, PREFERABLY WITH SOME SORT OF MESSAGE! */
+        http_response_code(403);
+        echo "Ouve um problema, tente novamente.";
+    } else {
+        // add this hash to the array
+        if (sizeof($_SESSION["hash"]) > 4) {
+            array_shift($_SESSION["hash"]);
+        }
+        array_push($_SESSION["hash"], $h);
+    }
+} else {
+    // create a hash array and add this hash
+    $_SESSION["hash"] = array(
+        $h
+    );
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     $address_size_bits = filter_var($_POST["address_size_bits"], FILTER_VALIDATE_INT);
     // echo 'Address' . $address_size_bits;
     $control_size_bits = filter_var($_POST["control_size_bits"], FILTER_VALIDATE_INT);
@@ -39,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // echo 'Offset: ' . $offset_size_bits;
     $way_size = filter_var($_POST["way_size"], FILTER_VALIDATE_INT);
     // echo 'Nway: ' . $way_size;
-
+    
     foreach ($_POST as $value) {
         if ($value < 0 or $value == null) {
             http_response_code(400);
@@ -47,19 +68,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         }
     }
-
+    
     try {
-
+        
         $cache = new Cache($address_size_bits, $control_size_bits, $tag_size_bits, $index_size_bits, $offset_size_bits, $way_size);
-
+        
         $calculadora = new CacheCalculadoraImpl();
-
+        
         $message = "Tamanho Total da Cache: " . $calculadora->calculaTamanhoCachePadrao($cache) . " bits";
-
+        
         // $message .= "\n\t" . json_encode($cache, JSON_PRETTY_PRINT) . "\n";
-
+        
         echo $message;
-
+        
         // echo ' Tamanho em bits: ' . $calculadora->calculaTamanhoCachePadraoParametrizado($cache->getAddressSizeBits(), $cache->getTagSizeBits(), $cache->getIndexSizeBits(), $cache->getOffsetSizeBits(), $cache->getControlSizeBits(), $cache->getWaysSize());
     } catch (Exception $e) {
         http_response_code(400);
